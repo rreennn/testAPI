4cc9e9af86012f82fb50d181742d03a8dc8275a5
rreennn <kerenkaren1704@gmail.com>
2025-01-09 18:13:54 +0700

    add sorting from newest post

--- controllers/postController.js
+++ controllers/postController.js
@@ -2,7 +2,7 @@ const Post = require("../models/post.js");
 
 const getPosts = async (req, res) => {
   try {
-    const posts = await Post.find({});
+    const posts = await Post.find({}).sort({ createdAt: -1 });
     res.status(200).json(posts);
   } catch (error) {
     res.status(500).json({ message: error.message });
@@ -84,7 +84,7 @@ const getUserPosts = async (req, res) => {
       return res.status(403).json({ message: "Unauthorized" });
     }
 
-    const posts = await Post.find({ author: req.user.username });
+    const posts = await Post.find({ author: req.user.username }).sort({ createdAt: -1 });
     res.status(200).json(posts);
   } catch (error) {
     res.status(500).json({ message: error.message });
