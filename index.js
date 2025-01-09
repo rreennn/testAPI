44932fdcb8cab23478afdeda41a06db24518b687
rreennn <kerenkaren1704@gmail.com>
2025-01-09 10:35:55 +0700

    update mongodb link

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const authRoute = require("./routes/authRoute.js");
-const postRoute = require("./routes/postRoute.js")
-const cors = require('cors');
+const postRoute = require("./routes/postRoute.js");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/posts", postRoute);
 app.use("/api/auth", authRoute);
@@ -19,7 +19,7 @@ app.get("/", (req, res) => {
 
 mongoose
   .connect(
-    "mongodb://keren:admin123@backenddb-shard-00-00.weqfs.mongodb.net:27017,backenddb-shard-00-01.weqfs.mongodb.net:27017,backenddb-shard-00-02.weqfs.mongodb.net:27017/?replicaSet=atlas-th08ld-shard-0&ssl=true&authSource=admin"
+    "mongodb://keren:admin123@backenddb-shard-00-00.weqfs.mongodb.net:27017,backenddb-shard-00-01.weqfs.mongodb.net:27017,backenddb-shard-00-02.weqfs.mongodb.net:27017/?ssl=true&replicaSet=atlas-th08ld-shard-0&authSource=admin&retryWrites=true&w=majority&appName=BackendDB"
   )
   .then(() => {
     console.log("Connected");
